774b476d10cc90d7991422d340831bae5ba586f1
Rene Loperena <reneloperena@Renes-mbp.local>
2020-05-26 14:11:25 -0500

    Remove console.log

--- property.js
+++ property.js
@@ -3,11 +3,10 @@ const { middleware } = require('./middleware')
 const fetch = require('node-fetch')
 
 async function findProperty (propertyId) {
-  console.log(propertyId)
   const response = await fetch(`https://www.example.org/property/${propertyId}`)
   const body = response.json()
   if (response.status >= 300) {
-    throw new Error(body.error)
+    throw new Error(`Property ${propertyId} not found`)
   }
   return body
 }
